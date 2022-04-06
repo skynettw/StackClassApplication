8a358a7b051e6c5018da0e64540f8689ab6a3eac
Richard <skynet.tw@gmail.com>
2022-04-06 20:14:58 +0800

    add pop command

--- Program.cs
+++ Program.cs
@@ -6,40 +6,6 @@ namespace MyAppClassTest1
     {
         static void Main(string[] args)
         {
-            //MyClass m = new (10, 15);
-
-            //m.display();
-            //Random rand = new Random();
-            //BMI bmi = new BMI(Convert.ToDouble(rand.Next(60,120)), Convert.ToDouble(rand.Next(150, 190)));
-            //Console.WriteLine(bmi.compute());
-            //string name = "Richard";
-            //Console.WriteLine("Hello, {0}, {1}", name, "You");
-            //Random random = new Random();    
-            //int answer = random.Next(1, 100);
-            //int guess=0;
-            //Console.WriteLine(answer);
-            //do
-            //{
-            //    Console.Write("Please guess a number:");
-            //    guess = Convert.ToInt32(Console.ReadLine());
-            //    if (guess == answer)
-            //    {
-            //        Console.WriteLine("You are right!");
-            //        break;
-            //    }
-            //    else
-            //    {
-            //        if (guess > answer)
-            //        {
-            //            Console.WriteLine("Too large, try again");
-            //        }
-            //        else
-            //        {
-            //            Console.WriteLine("Too small, try again");
-            //        }
-            //    }
-            //} while (true);
-            //Console.WriteLine("Thank you");
             MyStack stack = new MyStack();
             string cmd;
             int x, y;
@@ -50,6 +16,11 @@ namespace MyAppClassTest1
                 cmd = Console.ReadLine();
                 if (cmd == "quit" || cmd == "exit")
                     break;
+                else if (cmd == "pop")
+                {
+                    _ = stack.Pop();
+                    stack.Show();
+                }
                 else if (cmd == "show")
                 {
                     stack.Show();
